d4e0586cce751d1dc89198f91fb966985f9915ae
poyi555666-coder <poyi555666@gmail.com>
2026-03-30 12:02:42 +0800

    week05

--- week05/week06/week06_4b.cpp
+++
@@ -1,9 +0,0 @@
-// week06-4b. cpp SOIT_BASE_007
-#include <stdio.h>
-int main()
-{
-	int a, b;
-	scanf("%d%d", &a,&b);
-	printf("%d\n", a/b);
-
-}
--- week05/week06/week06_4c.cpp
+++
@@ -1,8 +0,0 @@
-//week06-4c.cpp SOIT107_Base_003
-#include <stdio.h>
-int main()
-{
-	int a, b;
-	scanf("%d%d", &a, &b);
-	printf("%d", a%b);
-}
